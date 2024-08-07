548bb9d9103ddd7ea0501b87f3b57cabb6a821ce
saikrishna <saikrishnavallamsetty123@gmail.com>
2024-08-07 11:14:44 +0530

    Subsequences from a List

--- src/com/sai/recursion/SubSequence.java
+++ src/com/sai/recursion/SubSequence.java
@@ -8,9 +8,57 @@ public class SubSequence {
        // subSequence("","abc");
        // System.out.println(subSequenceList("", "abc", new ArrayList<>()));
         //System.out.println(subSequenceList("", "abc"));
-        subSequenceAscii("", "abc");
+      //  subSequenceAscii("", "abc");
+        int[] arr = {1,2,3};
+        System.out.println(returnArrayList(arr));
     }
+    // Refer the incomplete code to revise 
+    /*static ArrayList<ArrayList<Integer>> returnArrayList(int[] arr) {
+        ArrayList<ArrayList<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
 
+        for(Integer num : arr) {
+            ArrayList<Integer> inner = new ArrayList<>();
+           *//* inner1.add(num);
+            outer.add(inner1);*//*
+            for(int i = 0; i <= outer.size();i++){
+                inner.add(arr[i]);
+                // ArrayList<Integer> inner = new ArrayList<>();
+                //inner.add(num);
+                //inner.add(arr[i]);
+                //  outer.add(inner);
+          *//*  for(int i = 0; i < arr.length - 1;i++){
+                ArrayList<Integer> arrayListt = new ArrayList<>();
+                arrayListt.add(arr[i]);
+                if(!outer.contains(arrayListt)){
+                    //arrLit.get(i).add(arr[i]);
+                    outer.add(arrayListt);
+                }*//*
+                // arrayList.add(arr[])
+              *//*  ArrayList<Integer> arrayList2 = new ArrayList<>();
+                arrayList2.add(arr[i]);
+                arrLit.add(arrayList2);
+                arrLit.get(i).add(arr[i]);*//*
+            }
+        }
+        return outer;
+    }*/
+
+    static ArrayList<ArrayList<Integer>> returnArrayList(int[] arr) {
+        ArrayList<ArrayList<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(Integer num : arr) {
+            int n = outer.size();
+            for(int i = 0; i < n;i++){
+                //taking the copy of the lists inside the outer list to add the num to it.
+                //suppose outer = [], [1] then we have to take the copy so that original will not get modified
+                ArrayList<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
     static void subSequence(String p, String unp) {
         if(unp.isEmpty()){
             System.out.println(p);
@@ -59,4 +107,7 @@ public class SubSequence {
 
         subSequenceAscii(p + (ch + 0),unp.substring(1));
     }
+
+
+
 }
